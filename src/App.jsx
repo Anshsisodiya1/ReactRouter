7983c234fd528121ebff8d16cf175267d55d8588
Ansh Sisodiya <sisodiyaansh785@gmail.com>
2024-10-06 13:40:31 -0400

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import './App.css'
+import RouterFile from "./RouterFile";
+
+
+const App = () => {
+  return (
+    <div>
+      <RouterFile/>
+    </div>
+  );
+};
+
+export default App;
